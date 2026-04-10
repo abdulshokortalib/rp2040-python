dcc6b4f98f38900edebf4bf9a8be60127e18d749
ABDUL SHOKOR BIN ABD TALIB <abdulshokor@gmail.com>
2026-04-10 16:41:54 +0800

    Update buzzer1.py

--- buzzer1.py
+++ buzzer1.py
@@ -1,25 +1,24 @@
 # =========================================================
 # Nama Fail   : buzzer1.py
-# Fungsi      : Menghidup dan memadam LED Grove secara berkelip walaupun nama fail merujuk kepada buzzer.
+# Fungsi      : Menghidup dan memadam buzzer.
 # Papan       : Maker Pi RP2040
 # Bahasa      : CircuitPython
-# Komponen    : Grove LED / LED digital
-# Pin Diguna  : LED: GP5
+# Komponen    : On Board Buzzer /Grove Buzzer
+# Pin Diguna  : Buzzer: GP22
 # Tahap       : Asas
-# Nota        : Nama fail tidak sepadan dengan fungsi sebenar kod. Fail ini tidak menggunakan buzzer.
+# Nota        : 
 # =========================================================
 
 import board
-import digitalio
+import pwmio
 import time
+import digitalio
 
-# LED guna pin Digital 
-led = digitalio.DigitalInOut(board.GP5)
-led.direction = digitalio.Direction.OUTPUT
+buzzer = pwmio.PWMOut(board.GP22, variable_frequency=True)
 
-while True:
-    led.value = True
-    time.sleep(1)
-    led.value = False
-    time.sleep(1)
+for i in range(3):
+    buzzer.duty_cycle = 32768
+    time.sleep(0.2)
+    buzzer.duty_cycle = 0
+    time.sleep(0.2)
     
